fc9df74f90fb32b6f2cb4f3543f029b6b00fa607
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-03-20 16:40:51 +0100

    finished the project with all the requirements

--- script.js
+++ script.js
@@ -3,26 +3,34 @@ const submitBtn = document.getElementById("submit-btn");
 const form = document.getElementById("form");
 const bookTitle = document.getElementById("book-title");
 const author = document.getElementById("author");
-// not sure this is needed
-class Book{
-    constructor(title, author){
-        this.title =  title;
-        this.author = author;
-    }
 
-//     addBook() {
-//     booklists.push({
-//     id: booklists.length + 1,
-//     title: this.title,
-//     author: this.author,
-// });
-//     }
-  saveToStorage() {
-    localStorage.setItem('bookLists', JSON.stringify(booklists));
-  } 
+// Storage
+
+function updateStorage(data) {
+  localStorage.setItem('bookLists', JSON.stringify(data));
+};
+function getLocalStorage (data){
+  JSON.parse(localStorage.getItem('booklist'));
+
 };
 
-//************************************* */
+function loadFromStorage() {
+  const storedBookList = localStorage.getItem('bookLists');
+  if (storedBookList) {
+      booklist = JSON.parse(storedBookList);
+      displayBooks();
+  }
+}
+
+
+//Remove title function
+
+function removeBook(title) {
+  booklist = booklist.filter(book => book.title !== title);
+  updateStorage(booklist);
+}
+
+// Function to display the book's list
 
 function displayBooks() {
   document.getElementById("book-list-container").innerHTML = "";
@@ -31,20 +39,32 @@ function displayBooks() {
     const booksTable = document.getElementById("book-list-container");
     bookItem.classList.add("book-item")
     bookItem.innerHTML = `
-        <p>${book.title}</p>
-        <p>${book.author}</p>
+        <p>Book title: ${book.title}</p>
+        <p>Author: ${book.author}</p>
         <button class="remove" id=${book.title}>Remove</button>
       <hr/>`;
       booksTable.appendChild(bookItem);
+
+      const removeBtn = bookItem.querySelector(".remove");
+    removeBtn.addEventListener("click", () => {
+      removeBook(book.title);
+      displayBooks();
+    });
+      
   });
 }
 
+// Function to add new books
 function addBooks (ttle, athr) {
   const book = {title:"", author: ""};
   book.title = ttle;
   book.author = athr;
-  booklist.push(book)
+  booklist.push(book);
+  updateStorage(booklist)
 }
+
+// Event listener to add books
+
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if(bookTitle.value !=="" && author.value !== ""){
@@ -54,10 +74,9 @@ submitBtn.addEventListener("click", (e) => {
   }else {
     alert("fill the required fields before submiting")
   }
-  
-  // let cntr = 0;
-  // const createdBook = new Book(bookTitle.value, author.value);
-  // console.log(createdBook)
-  // booklist.push(createdBook)
 });
+document.addEventListener("DOMContentLoaded", () => {
+  loadFromStorage();
+});
+
 
